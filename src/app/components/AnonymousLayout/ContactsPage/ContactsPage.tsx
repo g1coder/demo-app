8746f8bbd962c9dd4a797d6034bce2487caeda05
g1coder <2vsl@mail.ru>
2022-09-28 12:39:24 +0700

    change contact page

--- src/app/components/AnonymousLayout/ContactsPage/ContactsPage.tsx
+++ src/app/components/AnonymousLayout/ContactsPage/ContactsPage.tsx
@@ -3,7 +3,6 @@ import {StyledMainLayoutWrapper} from 'app/components/MainLayout/MainLayout';
 import {styled} from '@mui/material/styles';
 import {LANDING_PAGE_HEADER_HEIGHT} from 'app/constants/constants';
 import GeneralOfficeInfo from 'app/components/AnonymousLayout/ContactsPage/GeneralOfficeInfo';
-import {Typography} from '@mui/material';
 
 const StyledBackground = styled('section')({
   backgroundColor: 'white',
@@ -15,27 +14,11 @@ const StyledBackground = styled('section')({
   height: `calc(100% - ${LANDING_PAGE_HEADER_HEIGHT})`,
 });
 
-const StyledTitleContainer = styled('section')({
-  position: 'relative',
-  maxWidth: 600,
-  marginTop: 40,
-});
-
 const ContactsPage = () => {
   return (
     <StyledMainLayoutWrapper>
       <StyledBackground />
       <GeneralOfficeInfo />
-
-      <StyledTitleContainer>
-        <Typography variant="h3" color="primary.dark">
-          Sale and delivery points
-        </Typography>
-        <Typography variant="body2" color="primary.dark" sx={{marginTop: 2}}>
-          Sed sagittis sodales lobortis. Curabitur in eleifend turpis, id vehicula odio. Donec pulvinartellus eget magna
-          aliquet ultricies. Praesent gravida hendreritex, nec eleifend semconvallis vitae.
-        </Typography>
-      </StyledTitleContainer>
     </StyledMainLayoutWrapper>
   );
 };
